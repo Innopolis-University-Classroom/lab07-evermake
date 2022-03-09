460609c93eaecfbc46e91b56de188ad26ef23788
evermake <vladeryabkin@gmail.com>
2022-03-09 16:26:24 +0300

    solve exercise #02

--- ex02.hpp
+++ ex02.hpp
@@ -2,33 +2,29 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 
-public class Calculator {
-private:
-    int num1, num2;
+template<typename T>
+class Calculator {
+    T num1, num2;
 
-public:
-    Calculator(int n1, int n2) {
+  public:
+    Calculator(T n1, T n2) {
         num1 = n1;
         num2 = n2;
     }
 
-    int add() { return 0; /*your code here*/ }
-    int subtract() { /*your code here*/ }
-    int multiply() { /*your code here*/ }
-    int divide() { /*your code here*/ }
+    T add() {
+        return num1 + num2;
+    }
+
+    T subtract() {
+        return num1 - num2;
+    }
+
+    T multiply() {
+        return num1 * num2;
+    }
+
+    T divide() {
+        return num1 / num2;
+    }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
